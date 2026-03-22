a4c9a2f5b37435e805842e144469b0ba561147de
Maverick Liou <lliou@netskope.com>
2026-03-22 14:43:37 +0800

    Small refactor to the ff API client

--- src/api/client_feature_flag.py
+++ src/api/client_feature_flag.py
@@ -1,19 +1,22 @@
 import requests
 
 HOST_HEADER = {"Host": "provisioner-pycore-provisioner-tm"}
+API_PATH = "/client/config"
 
 
 def get_feature_flags(klbi: str, tenant_id: str) -> requests.Response:
     return requests.get(
-        url=f"http://{klbi}/client/config",
+        url=f"http://{klbi}{API_PATH}",
         params={"tenantid": tenant_id},
         headers=HOST_HEADER,
     )
 
 
-def set_feature_flag(klbi: str, tenant_id: str, flag: str, value: str) -> requests.Response:
+def set_feature_flag(
+    klbi: str, tenant_id: str, flag: str, value: str
+) -> requests.Response:
     return requests.post(
-        url=f"http://{klbi}/client/config",
+        url=f"http://{klbi}{API_PATH}",
         params={"tenantid": tenant_id},
         headers=HOST_HEADER,
         json={flag: value},
